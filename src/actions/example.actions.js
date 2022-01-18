b35405d71b0f1dae5d0dfe725293647f6030431d
David Archanjo <david.archanjo.ext@dasa.com.br>
2022-01-18 07:59:58 -0300

    first commit

---
+++ src/actions/example.actions.js
@@ -0,0 +1,36 @@
+import * as constants from '../constants/example.constants'
+import { createActions } from 'redux-actions'
+
+export const {
+  exampleConstants,  
+} = createActions({},
+  constants.EXAMPLE_CONSTANTS
+)
+
+// Async function call
+export const fetchAPI = (url) => ({
+  type: constants.API,
+  payload: {
+    url: 'url',
+    success: exampleConstants
+  }
+})
+
+export const fetchAPIPending = ( ) => ({
+  type: constants.FETCH_API_PENDING,  
+})
+
+export const fetchAPISuccess = (payload) => ({
+  type: constants.FETCH_API_SUCCESS,
+  content: payload
+})
+
+export const fetchAPIError = (payload) => ({
+  type: constants.FETCH_API_ERROR,
+  error: payload  
+})
+
+
+
+
+
